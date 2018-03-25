5897417f50c4524cb367db5b3e6a9fc8d49d979b
Daniel Ochoa <dannytenaglias@gmail.com>
2018-03-25 14:40:08 -0500

    Use :GoFmt vim and setup to auto fmt on file save.

--- src/horus/main.go
+++ src/horus/main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-    "net/http"
-    "encoding/json"
-    "fmt"
-    "io/ioutil"
-    "log"
-    "os"
-    "horus/utils"
-    "time"
+	"encoding/json"
+	"fmt"
+	"horus/utils"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
 )
+
 // Part One.
 // Horus
 
@@ -19,18 +20,17 @@ import (
 // 2. Check historical list against new request. If a new currency has been added:
 //   1. Send text alert of currency to list of available phones.
 
-    // we only need id and/or name to check.
-    // walk through each freshCurrency and check against matching pair in cached currencies. If we can't find a
-    // match, we have a new currency in gdax.
+// we only need id and/or name to check.
+// walk through each freshCurrency and check against matching pair in cached currencies. If we can't find a
+// match, we have a new currency in gdax.
 
 const (
-    gdaxUrl              = "https://api.gdax.com"
-    currenciesPath       = "/currencies"
-    cachedCurrenciesPath = "/data/currencies.json"
-    // for cache vs fresh request
-    freshData            = "fresh_data"
-    cachedData           = "cached_data"
-
+	gdaxUrl              = "https://api.gdax.com"
+	currenciesPath       = "/currencies"
+	cachedCurrenciesPath = "/data/currencies.json"
+	// for cache vs fresh request
+	freshData  = "fresh_data"
+	cachedData = "cached_data"
 )
 
 //
@@ -38,16 +38,16 @@ const (
 //
 
 type Currency struct {
-    Id      string `json:"id"`
-    Name    string `json:"name"`
-    Status  string `json:"status"`
-    MinSize string `json:"min_size"`
-    Message string `json:"message"`
+	Id      string `json:"id"`
+	Name    string `json:"name"`
+	Status  string `json:"status"`
+	MinSize string `json:"min_size"`
+	Message string `json:"message"`
 }
 
 type Currencies struct {
-    Collection     []Currency
-    CollectionType string
+	Collection     []Currency
+	CollectionType string
 }
 
 // TODO:
@@ -58,140 +58,140 @@ type Currencies struct {
 // - Abstract calls to exchange(s).
 
 func main() {
-    // use a ticker for this long running interval process
-    // TODO Make time in seconds a cli arg
-    timeInSeconds := time.Duration(10)
-    ticker := time.NewTicker(time.Second * timeInSeconds)
-    go func() {
-        go launchGDAXCurrencyCheck() // launch first..
-        fmt.Printf("Horus has been initiated with a ticker on %d seconds...\n", int(timeInSeconds))
-        for range ticker.C {
-            fmt.Printf("Horus: Launching currency check. Current time: %s\n", time.Now().Format(time.RFC1123))
-            go launchGDAXCurrencyCheck()
-        }
-    }()
+	// use a ticker for this long running interval process
+	// TODO Make time in seconds a cli arg
+	timeInSeconds := time.Duration(10)
+	ticker := time.NewTicker(time.Second * timeInSeconds)
+	go func() {
+		go launchGDAXCurrencyCheck() // launch first..
+		fmt.Printf("Horus has been initiated with a ticker of %d seconds...\n", int(timeInSeconds))
+		for range ticker.C {
+			fmt.Printf("Horus: Launching currency check. Current time: %s\n", time.Now().Format(time.RFC1123))
+			go launchGDAXCurrencyCheck()
+		}
+	}()
 
-    // prevent this process from ending.
-    keepRunning := make(chan interface{})
-    <-keepRunning
+	// prevent this process from ending.
+	keepRunning := make(chan interface{})
+	<-keepRunning
 }
 
 func launchGDAXCurrencyCheck() {
-    // paralelize goroutines
-    proc := make(chan Currencies, 2)
+	// paralelize goroutines
+	proc := make(chan Currencies, 2)
 
-    // freshCurrencies vs cachedCurrencies
-    go requestGdaxCurrencies(currenciesPath, proc)
-    go getCachedCurrencies(utils.GetGoPath() + cachedCurrenciesPath, proc)
+	// freshCurrencies vs cachedCurrencies
+	go requestGdaxCurrencies(currenciesPath, proc)
+	go getCachedCurrencies(utils.GetGoPath()+cachedCurrenciesPath, proc)
 
-    var callCount int
-    var cachedCurrencies, freshCurrencies Currencies
-    for currencies := range proc {
-        callCount++
-        switch collectionType := currencies.CollectionType; collectionType {
-        case cachedData:
-            cachedCurrencies = currencies
-        case freshData:
-            freshCurrencies = currencies
-        }
-        // _ = json.NewEncoder(os.Stdout).Encode(currencies.Collection)
-        if callCount == 2 {
-            close(proc)
-        }
-    }
+	var callCount int
+	var cachedCurrencies, freshCurrencies Currencies
+	for currencies := range proc {
+		callCount++
+		switch collectionType := currencies.CollectionType; collectionType {
+		case cachedData:
+			cachedCurrencies = currencies
+		case freshData:
+			freshCurrencies = currencies
+		}
+		// _ = json.NewEncoder(os.Stdout).Encode(currencies.Collection)
+		if callCount == 2 {
+			close(proc)
+		}
+	}
 
-    newCurrency, found := checkIfNewCurrencyFound(&cachedCurrencies, &freshCurrencies)
-    if found {
-        sendTwilioMessage(&newCurrency)
-        os.Exit(0)
-    } else {
-        fmt.Println("Horus: No new currencies found. Checking back later...")
-        fmt.Println()
-    }
+	newCurrency, found := checkIfNewCurrencyFound(&cachedCurrencies, &freshCurrencies)
+	if found {
+		sendTwilioMessage(&newCurrency)
+		os.Exit(0)
+	} else {
+		fmt.Println("Horus: No new currencies found. Checking back later...")
+		fmt.Println()
+	}
 }
 
 func sendTwilioMessage(newCurrency *Currency) {
-    // TODO: Implement this function and exit process entirely once it ocurs
-    fmt.Printf("Horus: sending notification that currency: %+v was just added..", newCurrency)
+	// TODO: Implement this function and exit process entirely once it ocurs
+	fmt.Printf("Horus: sending notification that currency: %+v was just added..", newCurrency)
 }
 
 func checkIfNewCurrencyFound(cachedCurrencies *Currencies, freshCurrencies *Currencies) (Currency, bool) {
-    if len(cachedCurrencies.Collection) == len(freshCurrencies.Collection) {
-        return Currency{}, false
-    }
-    fmt.Println()
-    fmt.Println("Horus: ========================== OMG | WE GOT A NEW COIN! | OMG ===========================")
-    fmt.Println()
-    // figure out which one is the new one..
-    // TODO: We'll have to write tests for this..
-    // Note that if more than 1 currency is added.. it doens't matter for now.
-    newCurrency, found := findNewlyAddedCurrency(cachedCurrencies, freshCurrencies)
-    if !found {
-        // we SHOULD have picked up a currency so obviously something went horribly wrong...
-        log.Fatal("Yeah dawg the `findNewlyAddedCurrency function is not working properly...`")
-        os.Exit(1)
-    }
-    return newCurrency, true
+	if len(cachedCurrencies.Collection) == len(freshCurrencies.Collection) {
+		return Currency{}, false
+	}
+	fmt.Println()
+	fmt.Println("Horus: ========================== OMG | WE GOT A NEW COIN! | OMG ===========================")
+	fmt.Println()
+	// figure out which one is the new one..
+	// TODO: We'll have to write tests for this..
+	// Note that if more than 1 currency is added.. it doens't matter for now.
+	newCurrency, found := findNewlyAddedCurrency(cachedCurrencies, freshCurrencies)
+	if !found {
+		// we SHOULD have picked up a currency so obviously something went horribly wrong...
+		log.Fatal("Yeah dawg the `findNewlyAddedCurrency function is not working properly...`")
+		os.Exit(1)
+	}
+	return newCurrency, true
 }
 
 // TODO: write tests - and more so this method.
 func findNewlyAddedCurrency(cachedCurrencies *Currencies, freshCurrencies *Currencies) (Currency, bool) {
-    // O + n^2
-    var freshCurrency, cachedCurrency Currency
-    var isThisNew bool
-    for i := 0; i < len(freshCurrencies.Collection); i++ {
-        freshCurrency = freshCurrencies.Collection[i]
-        isThisNew = true
-        for j := 0; j < len(cachedCurrencies.Collection); j++ {
-            cachedCurrency = cachedCurrencies.Collection[j]
-            // if there is no match between fresh and cached here, it means that is the new currency.
-            if freshCurrency.Id == cachedCurrency.Id {
-                isThisNew = false
-                break
-            }
-        }
-        if isThisNew {
-            return freshCurrency, true
-        }
-    }
-    return Currency{}, false
+	// O + n^2
+	var freshCurrency, cachedCurrency Currency
+	var isThisNew bool
+	for i := 0; i < len(freshCurrencies.Collection); i++ {
+		freshCurrency = freshCurrencies.Collection[i]
+		isThisNew = true
+		for j := 0; j < len(cachedCurrencies.Collection); j++ {
+			cachedCurrency = cachedCurrencies.Collection[j]
+			// if there is no match between fresh and cached here, it means that is the new currency.
+			if freshCurrency.Id == cachedCurrency.Id {
+				isThisNew = false
+				break
+			}
+		}
+		if isThisNew {
+			return freshCurrency, true
+		}
+	}
+	return Currency{}, false
 }
 
 func requestGdaxCurrencies(path string, proc chan<- Currencies) {
-    fmt.Printf("Horus: Requesting %s...\n", path)
-    res, getErr := http.Get(gdaxUrl + path)
-    if getErr != nil {
-        log.Fatal(getErr)
-        os.Exit(1)
-        // what to do..
-    }
-    defer res.Body.Close()
+	fmt.Printf("Horus: Requesting %s...\n", path)
+	res, getErr := http.Get(gdaxUrl + path)
+	if getErr != nil {
+		log.Fatal(getErr)
+		os.Exit(1)
+		// what to do..
+	}
+	defer res.Body.Close()
 
-    body, _         := ioutil.ReadAll(res.Body)
-    freshCurrencies := Currencies{CollectionType: freshData}
-    unmarshalErr    := json.Unmarshal(body, &freshCurrencies.Collection)
-    if unmarshalErr != nil {
-        log.Fatal(unmarshalErr)
-        os.Exit(1)
-    }
+	body, _ := ioutil.ReadAll(res.Body)
+	freshCurrencies := Currencies{CollectionType: freshData}
+	unmarshalErr := json.Unmarshal(body, &freshCurrencies.Collection)
+	if unmarshalErr != nil {
+		log.Fatal(unmarshalErr)
+		os.Exit(1)
+	}
 
-    proc<- freshCurrencies
+	proc <- freshCurrencies
 }
 
 func getCachedCurrencies(path string, proc chan<- Currencies) {
-    // defer use of a db. Just check data currencies.json file.
-    // TODO: figure out interfaces better so i can return err or nil. (?)
-    fmt.Println("Horus: Reading cached currencies ...")
-    content, err := ioutil.ReadFile(path)
-    if err != nil {
-        log.Fatal(err)
-    }
-    // todo we probably do need a store to write time of caching.
-    cachedCurrencies := Currencies{CollectionType: cachedData}
-    unmarshalErr := json.Unmarshal(content, &cachedCurrencies.Collection)
-    if unmarshalErr != nil {
-        log.Fatal(unmarshalErr)
-        os.Exit(1)
-    }
-    proc<- cachedCurrencies
+	// defer use of a db. Just check data currencies.json file.
+	// TODO: figure out interfaces better so i can return err or nil. (?)
+	fmt.Println("Horus: Reading cached currencies ...")
+	content, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// todo we probably do need a store to write time of caching.
+	cachedCurrencies := Currencies{CollectionType: cachedData}
+	unmarshalErr := json.Unmarshal(content, &cachedCurrencies.Collection)
+	if unmarshalErr != nil {
+		log.Fatal(unmarshalErr)
+		os.Exit(1)
+	}
+	proc <- cachedCurrencies
 }
--- src/horus/utils/main.go
+++ src/horus/utils/main.go
@@ -6,16 +6,16 @@ import "os"
 // Get $GOPATH for use in opening files for example. Since we may be setting the path to use `:` for multiple
 // lookups, we need to split that up. It returns the first path without the string 'bin' in it.
 func GetGoPath() string {
-    if strings.Contains(os.Getenv("GOPATH"), ":") {
-        paths := strings.Split(os.Getenv("GOPATH"), ":")
-        for _, path := range paths {
-            if strings.Contains(path, "bin") {
-                continue;
-            }
-            return path
-        }
-        // all paths contain bin?!?! whatever.
-        return paths[0]
-    }
-    return os.Getenv("GOPATH")
+	if strings.Contains(os.Getenv("GOPATH"), ":") {
+		paths := strings.Split(os.Getenv("GOPATH"), ":")
+		for _, path := range paths {
+			if strings.Contains(path, "bin") {
+				continue
+			}
+			return path
+		}
+		// all paths contain bin?!?! whatever.
+		return paths[0]
+	}
+	return os.Getenv("GOPATH")
 }
